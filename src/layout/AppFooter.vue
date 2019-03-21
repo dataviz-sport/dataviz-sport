9a343f5ff6d1f67f1d1bcd3b683d923ed46f9e4d
Maxime AUBLET <maximeaublet@gmail.com>
2019-03-21 18:54:19 +0100

    maj

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -1,55 +1,50 @@
 <template>
-    <footer class="footer has-cards">
-        <div class="container">
-            <div class="row align-items-center justify-content-md-between">
-                <div class="col-md-6">
-                    <div class="copyright">
-                        Copyright&nbsp;&copy; {{year}}
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <ul class="nav nav-footer justify-content-end">
-                        <li class="nav-item">
-                            <a href="https://www.ozae.com/fr/" class="nav-link" target="_blank" rel="noopener">Ozae</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="row align-items-center justify-content-md-between">
-                <div class="col-md-6">
-                    Hackathon Ges 2019
-                    <br>
-                    Groupe 26
-                </div>
-                <div class="col-md-6">
-                    <ul class="nav nav-footer justify-content-end">
-                        <li class="nav-item">
-                            <a href="https://www.creative-tim.com" class="nav-link" target="_blank" rel="noopener">Creative Tim</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank" rel="noopener">About
-                                Us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="http://blog.creative-tim.com" class="nav-link" target="_blank" rel="noopener">Blog</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="https://github.com/creativetimofficial/argon-design-system/blob/master/LICENSE.md"
-                               class="nav-link" target="_blank" rel="noopener">MIT License</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+  <footer class="footer has-cards">
+    <div class="container">
+      <div class="row align-items-center justify-content-md-between">
+        <div class="col-md-6">
+          <ul class="nav nav-footer justify-content-start">
+            <li class="nav-item">
+              <a
+                href="https://github.com/dataviz-sport"
+                class="nav-link"
+                target="_blank"
+                rel="noopener"
+              >Hackathon GES 2019 - DataViz Sport {{year}}</a>
+            </li>
+            <li class="nav-item">
+              <a
+                href="https://github.com/dataviz-sport"
+                class="nav-link"
+                target="_blank"
+                rel="noopener"
+              >Groupe 26 : Aublet - Demay - Kok - Gibbings - Razonable</a>
+            </li>
+          </ul>
         </div>
-    </footer>
+        <div class="col-md-6">
+          <ul class="nav nav-footer justify-content-end">
+            <li class="nav-item">
+              <a
+                href="https://github.com/creativetimofficial/argon-design-system"
+                class="nav-link"
+                target="_blank"
+                rel="noopener"
+              >Template from : Creative Tim</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 <script>
 export default {
-  name: 'app-footer',
+  name: "app-footer",
   data() {
     return {
       year: new Date().getFullYear()
-    }
+    };
   }
 };
 </script>
